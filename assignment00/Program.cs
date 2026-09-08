df653e0c2c862ca42354a3985f1f51127849e161
Engmohamed-ali05 <bad100899@gmail.com>
2026-09-08 16:42:12 +0300

    Question 2

--- assignment00/Program.cs
+++ assignment00/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace assignment00
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace assignment00
 {
     internal class Program
     {
@@ -8,10 +10,26 @@
             #region   Q1        
             // Q1:1-Create a one-dimensional array double[] prices with the values 25.5, 40.0, 33.75. Print the second price(index 1).
 
-                double[] prices = { 25., 40.0, 33.75 };
-             
-            Console.WriteLine(prices[1]);
+            ///     double[] prices = { 25., 40.0, 33.75 };
+
+            ///Console.WriteLine(prices[1]);
+            #endregion
+
+            #region
+            ///  Q2 : 2-Create a 2x2 multidimensional array int[,] shelfCopies where shelf 0 has 3, 5 copies and
+            /// shelf 1 has 1, 4 copies.Print the number of copies on shelf 1, slot 0.
+
+            int[,] shelfcopies =
+                 {
+                    { 3, 5 }, { 1, 4 }
+                                    
+                 }; 
+
+            Console.WriteLine ( shelfcopies [1, 0] );
+
             #endregion 
+
+
 
 
 
